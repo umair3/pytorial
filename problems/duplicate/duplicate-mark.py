52bb983055af778db23e615614910544983e458a
Umair Anwar <umair.anwr@gmail.com>
2019-12-26 16:15:27 +0500

    Find duplicate by marking

---
+++ problems/duplicate/duplicate-mark.py
@@ -0,0 +1,13 @@
+
+def find_duplicate(a):
+    for i in range(0, len(a)):
+        index = abs(a[i])
+        if a[index] < 0:
+            return index
+        else:
+            a[index] = a[index] * -1
+    return -1
+
+
+print(find_duplicate([1, 2, 3, 4, 2]))
+print(find_duplicate([1, 2, 3, 4, 0]))
